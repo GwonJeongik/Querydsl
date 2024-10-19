3f5cc74136cd4686ade11b75eb0b421ededb2c56
ik <jeongik11029@naver.com>
2024-10-20 00:39:47 +0900

    [test] : Querydsl 환경설정 검증 QTestEntity 테스트

--- src/test/java/query/dsl/DslApplicationTests.java
+++ src/test/java/query/dsl/DslApplicationTests.java
@@ -1,13 +1,36 @@
 package query.dsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import query.dsl.entity.QTestEntity;
+import query.dsl.entity.TestEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class DslApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    void contextLoads() {
+        TestEntity testEntity = new TestEntity();
+        em.persist(testEntity);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QTestEntity qTestEntity = QTestEntity.testEntity;
+
+        TestEntity result = query.selectFrom(qTestEntity)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(testEntity);
+        assertThat(result.getId()).isEqualTo(testEntity.getId());
+    }
 
 }
